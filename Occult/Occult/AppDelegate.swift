b2dda89560b6c13cc98bdebed4bdbb09c4ceba52
Don Michele Bigi <mikbigi@gmail.com>
2018-08-10 15:15:01 +0200

    Occult 0.0 initial

---
+++ Occult/Occult/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Occult
+//
+//  Created by Michele Bigi on 10/08/18.
+//  Copyright © 2018 Michele Bigi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Occult/Occult/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Occult
+//
+//  Created by Michele Bigi on 10/08/18.
+//  Copyright © 2018 Michele Bigi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
